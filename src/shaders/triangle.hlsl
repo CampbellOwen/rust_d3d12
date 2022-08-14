8b5e065498a498009ce03a4b534a127da36c5d34
Owen Campbell <ocampbell95@gmail.com>
2022-08-14 10:07:06 -0700

    Add light falloff

--- src/shaders/triangle.hlsl
+++ src/shaders/triangle.hlsl
@@ -15,16 +15,21 @@ PSInput VSMain(float3 position : POSITION, float3 normal : NORMAL, float2 uv : T
 
     float4 pos_world = mul(M, float4(position, 1.0));
 
-    float3 normal_world = mul(M, float4(normal, 0.0)).xyz;
+    float3 normal_world = mul(M, float4(normal, 0.0)).xyz; // Use 0.0 because normal is a bivector
 
-    float3 l = normalize(float3(2.0, 2.0, 0.0) - pos_world);
+    float3 l = float3(2.0, 2.0, 0.0) - pos_world;
+    float l_dist = length(l);
+    l = normalize(l);
     float ldotn = clamp(dot(l, normalize(normal_world)), 0.0, 1.0);
 
     float4 pos_clip = mul(P, pos_world);
 
+    float4 light_col = float4(10.0, 0.0, 0.0, 1.0);
+
+    light_col *= (1 / (l_dist * l_dist));
+
     result.position = pos_clip;
-    //result.color = mul(m, color);
-    result.color = float4(0.9, 0.9, 0.9, 1.0) * ldotn;
+    result.color = float4(0.9, 0.9, 0.9, 1.0) * (ldotn * light_col) / 3.14159;
 
     return result;
 }
