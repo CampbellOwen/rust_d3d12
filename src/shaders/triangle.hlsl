5b17851432afad1d61dd68a2e511b5d66fa8a56b
Owen Campbell <ocampbell95@gmail.com>
2022-08-13 18:27:55 -0700

    Add lighting

--- src/shaders/triangle.hlsl
+++ src/shaders/triangle.hlsl
@@ -14,11 +14,17 @@ PSInput VSMain(float3 position : POSITION, float3 normal : NORMAL, float2 uv : T
     PSInput result;
 
     float4 pos_world = mul(M, float4(position, 1.0));
+
+    float3 normal_world = mul(M, float4(normal, 0.0)).xyz;
+
+    float3 l = normalize(float3(2.0, 2.0, 0.0) - pos_world);
+    float ldotn = clamp(dot(l, normalize(normal_world)), 0.0, 1.0);
+
     float4 pos_clip = mul(P, pos_world);
 
     result.position = pos_clip;
     //result.color = mul(m, color);
-    result.color = float4(0.2, 0.2, 0.2, 1.0);
+    result.color = float4(0.9, 0.9, 0.9, 1.0) * ldotn;
 
     return result;
 }
