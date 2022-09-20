4825dce055ec163692e0bd9fc1cc02a3df3d9007
Owen Campbell <ocampbell95@gmail.com>
2022-09-20 11:58:47 -0700

    Move lighting to pixel shader

--- renderer/src/shaders/triangle.hlsl
+++ renderer/src/shaders/triangle.hlsl
@@ -8,7 +8,8 @@ SamplerState s1 : register(s0);
 struct PSInput
 {
     float4 position : SV_POSITION;
-    float4 color : COLOR;
+    float4 position_world : POSITION;
+    float3 normal : NORMAL;
     float2 uv : TEXCOORD;
 };
 
@@ -18,21 +19,10 @@ PSInput VSMain(float3 position : POSITION, float3 normal : NORMAL, float2 uv : T
 
     float4 pos_world = mul(M, float4(position, 1.0));
 
-    float3 normal_world = mul(M, float4(normal, 0.0)).xyz; // Use 0.0 because normal is a bivector
-
-    float3 l = float3(2.0, 2.0, -1.0) - pos_world.xyz;
-    float l_dist = length(l);
-    l = normalize(l);
-    float ldotn = clamp(dot(l, normalize(normal_world)), 0.0, 1.0);
-
     float4 pos_clip = mul(P, pos_world);
-
-    float4 light_col = float4(20.0, 20.0, 20.0, 1.0);
-
-    light_col *= (1 / (l_dist * l_dist));
-
     result.position = pos_clip;
-    result.color = float4(0.9, 0.9, 0.9, 1.0) * (ldotn * light_col) / 3.14159;
+    result.position_world = pos_world;
+    result.normal = normal;
     result.uv = uv;
     //result.color = t1.Sample(s1, uv);
 
@@ -41,5 +31,16 @@ PSInput VSMain(float3 position : POSITION, float3 normal : NORMAL, float2 uv : T
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-    return input.color * t1.Sample(s1, input.uv);
+    float3 normal_world = mul(M, float4(input.normal, 0.0)).xyz; // Use 0.0 because normal is a bivector
+
+    float3 l = float3(2.0, 2.0, -1.0) - input.position_world.xyz;
+    float l_dist = length(l);
+    l = normalize(l);
+    float ldotn = clamp(dot(l, normalize(normal_world)), 0.0, 1.0);
+
+    float4 light_col = float4(20.0, 20.0, 20.0, 1.0);
+
+    light_col *= (1 / (l_dist * l_dist));
+
+    return t1.Sample(s1, input.uv) * (ldotn * light_col) / 3.14159;
 }
